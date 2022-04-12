37d21cdd869976bf0ab71561db15d1c137ccb33b
Manny Lee <many221@gmail.com>
2022-04-11 21:53:52 -0700

    initial commit

---
+++ src/test/java/com/gr_20/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr_20.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
